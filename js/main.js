cb480de5f944fa9d97b82b8ca2c2216c4fced2e9
missserious <Julia_Ernst@gmx.de>
2022-05-26 16:52:02 +0200

    fade in job description

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
     document.getElementById('my-name').classList.add('fade-in');
+    document.getElementById('my-job').classList.add('fade-in');
   
 
     const header = document.querySelector('header');
