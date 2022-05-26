f81f5e1a57ad0de9bc16b706a995cfb744cc3b31
missserious <Julia_Ernst@gmx.de>
2022-05-26 16:32:51 +0200

    fade in header image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // fade in effect for header
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
-    document.getElementById("my-name").classList.add('fade-in');
+    document.getElementById('my-name').classList.add('fade-in');
+  
+
+    const header = document.querySelector('header');
+    header.className += 'fade-in-header';
+
 });
